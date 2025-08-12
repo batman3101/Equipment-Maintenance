684c79e7b227b2a4f721dc0b2906985a22bb8b5a
PARK YOUNG IL <zetooo1972@gmail.com>
2025-08-12 12:02:02 +0700

    배포 테스트 22

--- src/types/react-dom.d.ts
+++ src/types/react-dom.d.ts
@@ -1,10 +1,23 @@
-import type * as React from 'react'
+declare module "react-dom" {
+  import * as React from "react";
 
-declare module 'react-dom' {
   export function createPortal(
     children: React.ReactNode,
-    container: Element | DocumentFragment
-  ): React.ReactPortal
+    container: Element | DocumentFragment,
+    key?: null | string
+  ): React.ReactPortal;
+
+  export function render(
+    element: React.ReactElement,
+    container: Element | null,
+    callback?: () => void
+  ): void;
+
+  export function unmountComponentAtNode(container: Element | null): boolean;
+
+  export function findDOMNode(
+    componentOrElement: Element | React.Component<any, any> | null
+  ): Element | Text | null;
+
+  export const version: string;
 }
-
-
